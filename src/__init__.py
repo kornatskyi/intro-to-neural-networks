88263c87014de5812856c7ef7c13b7ea7b4376b7
Bohdan Kornatskyi <bohdan.kornatskyi@gmail.com>
2023-09-22 18:29:52 -0700

    fix notebooks

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,3 @@
 from .Value import Value
 from .NeuralNetwork import Neuron, Layer, MLP
 from .utilities import draw_dot, trace
-from .Data import testing_data_points, training_data_points, testing_targets, training_targets
